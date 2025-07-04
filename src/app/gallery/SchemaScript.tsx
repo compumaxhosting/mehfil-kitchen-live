446f96d4b9b37ce7db402a0718094d5909cbfe01
Mohammed Sarfaraz <sarfarazuddin9999@gmail.com>
2025-07-04 21:29:42 +0530

    fixed schema issue

---
+++ src/app/gallery/SchemaScript.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Script from 'next/script'
+
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "Restaurant",
+  "name": "Mehfil Kitchen",
+  "image": "https://www.mehfilkitchen.com/logo2.png",
+  "@id": "https://www.mehfilkitchen.com/",
+  "url": "https://www.mehfilkitchen.com/",
+  "telephone": "+1-786-763-4000",
+  "email": "info@mehfilkitchen.com",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "5264 Lincoln Ave",
+    "addressLocality": "Skokie",
+    "addressRegion": "IL",
+    "postalCode": "60077",
+    "addressCountry": "US"
+  },
+  "priceRange": "$$",
+  "servesCuisine": ["Indian", "Pakistani", "Desi", "Halal"],
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      "opens": "11:00",
+      "closes": "22:00"
+    }
+  ],
+  "sameAs": [
+    "https://www.facebook.com/mehfilkitchen/",
+    "https://www.instagram.com/kitchen.mehfil/?igsh=ejNmNzJkbG56bmxj#/"
+  ]
+}
+
+export default function SchemaScript() {
+  return (
+    <Script
+      id="gallery-schema"
+      type="application/ld+json"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+    />
+  )
+}
--- src/app/gallery/layout.tsx
+++ src/app/gallery/layout.tsx
@@ -1,7 +1,5 @@
-'use client'
-
-import React from 'react'
-import Script from 'next/script'
+import React from 'react'// Import client component
+import SchemaScript from './SchemaScript'
 
 export const metadata = {
   title: 'Mehfil Kitchen Photo Gallery | Indian & Hyderabadi Cuisine in Skokie',
@@ -21,56 +19,10 @@ export const metadata = {
   ],
 }
 
-const schema = {
-  "@context": "https://schema.org",
-  "@type": "Restaurant",
-  "name": "Mehfil Kitchen",
-  "image": "https://www.mehfilkitchen.com/logo2.png",
-  "@id": "https://www.mehfilkitchen.com/",
-  "url": "https://www.mehfilkitchen.com/",
-  "telephone": "+1-786-763-4000",
-  "email": "info@mehfilkitchen.com",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "5264 Lincoln Ave",
-    "addressLocality": "Skokie",
-    "addressRegion": "IL",
-    "postalCode": "60077",
-    "addressCountry": "US"
-  },
-  "priceRange": "$$",
-  "servesCuisine": ["Indian", "Pakistani", "Desi", "Halal"],
-  "openingHoursSpecification": [
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-      ],
-      "opens": "11:00",
-      "closes": "22:00"
-    }
-  ],
-  "sameAs": [
-    "https://www.facebook.com/mehfilkitchen/",
-    "https://www.instagram.com/kitchen.mehfil/?igsh=ejNmNzJkbG56bmxj#/"
-  ]
-}
-
 export default function GalleryLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Script
-        id="gallery-schema"
-        type="application/ld+json"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-      />
+      <SchemaScript />
       {children}
     </>
   )
