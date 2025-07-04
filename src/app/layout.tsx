2463943e88c861176ca6a9d9c9ae09b54dff40cb
Mohammed Sarfaraz <sarfarazuddin9999@gmail.com>
2025-07-04 20:51:47 +0530

    schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Josefin_Sans } from 'next/font/google'
+import Head from 'next/head'
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -25,9 +26,56 @@ export const metadata = {
   ],
 }
 
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "Restaurant",
+  "name": "Mehfil Kitchen",
+  "image": "https://www.mehfilkitchen.com/assets/images/logo.png",
+  "@id": "https://www.mehfilkitchen.com/",
+  "url": "https://www.mehfilkitchen.com/",
+  "telephone": "+1-786-763-4000",
+  "email": "info@mehfilkitchen.com",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "5264 Lincoln Ave",
+    "addressLocality": "Skokie",
+    "addressRegion": "IL",
+    "postalCode": "60077",
+    "addressCountry": "US"
+  },
+  "priceRange": "$$",
+  "servesCuisine": ["Indian", "Pakistani", "Desi", "Halal"],
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      "opens": "11:00",
+      "closes": "22:00"
+    }
+  ],
+  "sameAs": [
+    "https://www.facebook.com/mehfilkitchen/",
+    "https://www.instagram.com/kitchen.mehfil/?igsh=ejNmNzJkbG56bmxj#/"
+  ]
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={josefin.variable}>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </Head>
       <body>{children}</body>
     </html>
   )
