f0290902915dee7ab4236a63bee93fcd6ac4d5ce
Mohammed Sarfaraz <sarfarazuddin9999@gmail.com>
2025-07-15 23:19:59 +0530

    test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ export default function AboutSection() {
                 <div className="md:w-1/2 text-left">
                     <h2 className="text-[33px] sm:text-[36px] text-center font-semibold mb-6 text-gray-800">
                         About Mehfil Kitchen
-                        {/* New comment to trigger rebuild */}
+                       
                     </h2>
                     <div className="text-[16px] text-gray-700 space-y-2 leading-relaxed">
                         <p>
